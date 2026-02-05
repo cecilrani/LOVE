be8f5d12f82af17a131a39aad08d0dbab1ea7274
cecilrani <cecilrani10@gmail.com>
2026-02-05 21:53:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("runBtn");
+const msg = document.getElementById("message");
+
+// Move button randomly when mouse comes near
+btn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 100);
+  btn.style.left = x + "px";
+  btn.style.top = y + "px";
+});
+
+// If user manages to click
+btn.addEventListener("click", () => {
+  msg.textContent = "😲 Wow! You caught me!";
+});
